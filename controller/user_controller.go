ad56b61235fd8d35ec059a4bbdc86b1ec5de4f18
Cương Đặng Trung <cuongisstudying@gmail.com>
2022-06-14 11:54:39 +0700

    fix deploy heroku

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -26,6 +26,17 @@ type UserController interface {
 // @Router /user/all [get]
 func (c *userController) GetAll(w http.ResponseWriter, r *http.Request) {
 	var res *model.Response
+	users, err := c.userService.GetAll()
+	if err != nil {
+		internalServerErrorResponse(w, r, err)
+		return
+	}
+
+	res = &model.Response{
+		Data:    users,
+		Message: "OK",
+		Success: true,
+	}
 	render.JSON(w, r, res)
 }
 
@@ -33,4 +44,26 @@ func NewUserController() userController {
 	return userController{
 		userService: service.NewUserService(),
 	}
+}
+
+func internalServerErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	http.Error(w, err.Error(), http.StatusInternalServerError)
+	w.WriteHeader(http.StatusInternalServerError)
+	render.JSON(w, r, &model.Response{
+		Data:    nil,
+		Message: err.Error(),
+		Success: false,
+	})
+	return
+}
+
+func badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	http.Error(w, err.Error(), http.StatusBadRequest)
+	w.WriteHeader(http.StatusBadRequest)
+	render.JSON(w, r, &model.Response{
+		Data:    nil,
+		Message: err.Error(),
+		Success: false,
+	})
+	return
 }
