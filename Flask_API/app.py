53ccc9f8a0f9ef392f46de6e887f703d20c32b95
datntrong <datntrong@gmail.com>
2022-06-20 02:38:41 +0700

    call frontend to backend

--- Flask_API/app.py
+++ Flask_API/app.py
@@ -1,6 +1,7 @@
-import torch
-from flask import Flask, request
-from flask_cors import CORS, cross_origin
+import json
+
+
+import urllib.request
 from model import *
 import audioread
 from flask import Flask, request #import main Flask class and request object
@@ -12,13 +13,13 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('HELLO WORLD')
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = load_model('models/resnet_50_0605_1007_r2.t7')
+model = model.to(device)
 app = Flask(__name__)
 cors = CORS(app)
 
 # load model
-model = load_model('models/resnet_50_0605_1007_r2.t7')
 
-model = model.to(device)
 
 # @app.route('/')
 # def landing():
@@ -37,9 +38,10 @@ def read_audio_file(audio_from_post):
 
 
 def prediction_gender(wav):
+
     model.eval()
     feats = gen_spec(wav, duration=2)
-    inputs = torch.Tensor(feats)
+    inputs = torch.tensor(feats)
     inputs = inputs[None,:]
     inputs = inputs.to(device)
     outputs = model(inputs)
@@ -51,46 +53,29 @@ def prediction_gender(wav):
 
 
 
+def getjsonreturn():
+    gender = prediction_gender('1.wav')
+    if(gender ==1):
+        gender = 'male'
+    else:
+        gender = 'female'
+    return gender
+
+
 
 
 # Get the blob of type "audio/webm;codecs=opus"
-@app.route('/audio_record', methods=['POST'])
+@app.route('/audio_record', methods=['GET', 'POST'])
 def save_record():
-    logger.info("welcome to upload`")
-    # file = request.files['file']
-    #filename = secure_filename(file.title)
-
-    file = request.form['file']
-
-    print('File from the POST request is: {}'.format(file))
+    print('Done')
+    file = request.files['file']
     try:
-        read_audio_file(file[0])
-        return "****** Audio Read ******"
+        audio = request.files["file"]
+        path = '1.wav'
+        audio.save(path)
     except:
-        print("In the except", file[0]) # Gets printed as undefined
-        title = request.form['title']
-        print(title) # Able to print title
-        return "Request received and responded"
-        # app.logger.debug(request.files['file'].filename)
+        print('B')
 
-
-
-
-
-# @app.route("/predict", methods=["GET", "POST"])
-# def predictcrop():
-#     try:
-#         if request.method == "POST":
-#             form_values = request.form.to_dict()
-#             column_names = ["N", "P", "K", "temperature", "humidity", "ph", "rainfall"]
-#             input_data = np.asarray([float(form_values[i].strip()) for i in column_names]).reshape(
-#                 1, -1
-#             )
-#             prediction_data = crop_prediction(input_data)
-#             json_obj = json.dumps(prediction_data, default=convert)
-#             return json_obj
-#     except:
-#         return json.dumps({"error":"Please Enter Valid Data"}, default=convert)
 
 
 
@@ -104,5 +89,5 @@ def hello_world():
 
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
